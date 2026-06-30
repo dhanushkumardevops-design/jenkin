caee0af71164f5f5f31fb5db14696dad067f97b5
dhanushkumardevops-design <dhanushkumardevops@gmail.com>
2026-06-30 12:07:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
 package com.example;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Simple Java Project ===");
-
         // Use Calculator
         Calculator calc = new Calculator();
         System.out.println("5 + 3 = " + calc.add(5, 3));
         System.out.println("10 - 4 = " + calc.subtract(10, 4));
         System.out.println("6 * 7 = " + calc.multiply(6, 7));
-        System.out.println("20 / 4 = " + calc.divide(20, 4));
-
+        System.out.println("20 / 4 = " + calc.divide(20, 0));
         // Use Greeter
         Greeter greeter = new Greeter("World");
         greeter.greet();
